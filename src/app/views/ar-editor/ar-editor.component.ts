eac0cf7373a648f7e9a349e5f8ff61f280694dee
Ian V. Tatiana <ian.tatiana.v@gmail.com>
2021-02-08 02:12:58 +1000

    IMport threejs, add canvas and left-sidebar

--- src/app/views/ar-editor/ar-editor.component.ts
+++ src/app/views/ar-editor/ar-editor.component.ts
@@ -1,15 +1,96 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, ElementRef, Component, OnInit, Input, ViewChild } from '@angular/core';
+import * as THREE from 'three'
 
 @Component({
   selector: 'app-ar-editor',
   templateUrl: './ar-editor.component.html',
   styleUrls: ['./ar-editor.component.css']
 })
-export class ArEditorComponent implements OnInit {
+export class ArEditorComponent implements AfterViewInit {
+
+  private camera: THREE.PerspectiveCamera;
+
+  private get canvas() : HTMLCanvasElement {
+    return this.canvasRef.nativeElement;
+  }
+
+  @ViewChild('canvas')
+  private canvasRef: ElementRef;
+
+  private cube: THREE.Mesh;
+  private scene: THREE.Scene;
+  private renderer: THREE.WebGLRenderer;
+
+  @Input()
+  public rotationSpeedX: number = 0.005;
+
+  @Input()
+  public rotationSpeedY: number = 0.01;
+
+  @Input()
+  public size: number = 200;
+
+  @Input()
+  public cameraZ: number = 3;
+
+  @Input()
+  public fieldOfView: number = 70;
+
+  @Input('nearClipping')
+  public nearClippingPane: number = 1;
+
+  @Input('farClipping')
+  public farClippingPane: number = 1000;
+
+  private animateCube() {
+    this.cube.rotation.x += this.rotationSpeedX;
+    this.cube.rotation.y += this.rotationSpeedY;
+  }
+
+  private createScene() {
+    this.scene = new THREE.Scene();
+
+    let aspectRatio = this.getAspectRatio();
+    this.camera = new THREE.PerspectiveCamera(
+      this.fieldOfView,
+      aspectRatio,
+      this.nearClippingPane,
+      this.farClippingPane
+    );
+    this.camera.position.z = this.cameraZ;
+
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    this.cube = new THREE.Mesh(geometry, material);
+    this.scene.add(this.cube);
+  }
+
+  private getAspectRatio() {
+    return this.canvas.clientWidth / this.canvas.clientHeight;
+  }
+
+  private startRenderLooping() {
+    this.renderer = new THREE.WebGLRenderer({
+      canvas: this.canvas
+    });
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+   // this.renderer.setSize(this.canvas.clientWidth, this.canvas.clientHeight);
+    document.body.appendChild( this.renderer.domElement );
+
+    let component: ArEditorComponent = this;
+    (function renderer(){
+      requestAnimationFrame(renderer);
+      component.animateCube();
+      component.renderer.render(component.scene, component.camera);
+    }());
+  }
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.createScene();
+    this.startRenderLooping();
   }
 
 }
