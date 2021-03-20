371f0e138c74afeae69b19f4e4dd84b790ac81df
Ian V. Tatiana <ian.tatiana.v@gmail.com>
2021-03-21 03:33:10 +1000

    Editor: Add upload image button with preview

--- src/app/components/editor-toolbar/editor-toolbar.component.ts
+++ src/app/components/editor-toolbar/editor-toolbar.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditorToolbarComponent implements OnInit {
 
+  url = '';
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSelectFile(event) {
+    if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
+
+      reader.readAsDataURL(event.target.files[0]); // read file as data url
+
+      reader.onload = (event) => { // called once readAsDataURL is completed
+        this.url = event.target.result as string;
+      }
+    }
+  }
 }
